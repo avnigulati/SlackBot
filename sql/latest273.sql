c1aa90cb75afc53990a990806bfc6f594e23abd8
Avni Gulati <avni.gulati@sjsu.edu>
2017-12-05 12:06:45 -0800

    lastest sql added

---
+++ sql/latest273.sql
@@ -0,0 +1,106 @@
+CREATE DATABASE  IF NOT EXISTS `slackbot` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `slackbot`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: slackbot
+-- ------------------------------------------------------
+-- Server version	5.7.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `RoomType`
+--
+
+DROP TABLE IF EXISTS `RoomType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `RoomType` (
+  `idRoomType` int(11) NOT NULL,
+  `Description` longtext,
+  `NoOfRooms` int(11) DEFAULT NULL,
+  `RentPerNight` int(11) DEFAULT NULL,
+  `Type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idRoomType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `RoomType`
+--
+
+LOCK TABLES `RoomType` WRITE;
+/*!40000 ALTER TABLE `RoomType` DISABLE KEYS */;
+INSERT INTO `RoomType` VALUES (1,'deluxe room 2 beds, one restroom, room service available',25,100,'deluxe'),(2,'sweet room 2 bed',50,150,'suite'),(3,'condo room 5 bed',10,2000,'condo');
+/*!40000 ALTER TABLE `RoomType` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bookings`
+--
+
+DROP TABLE IF EXISTS `bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bookings` (
+  `username` varchar(20) NOT NULL,
+  `bookingId` int(11) NOT NULL AUTO_INCREMENT,
+  `roomType` varchar(45) DEFAULT NULL,
+  `checkIn` date DEFAULT NULL,
+  PRIMARY KEY (`bookingId`,`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookings`
+--
+
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+INSERT INTO `bookings` VALUES ('avni',1,'suite','2018-04-23');
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `currentuser`
+--
+
+DROP TABLE IF EXISTS `currentuser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `currentuser` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `currentuser`
+--
+
+LOCK TABLES `currentuser` WRITE;
+/*!40000 ALTER TABLE `currentuser` DISABLE KEYS */;
+INSERT INTO `currentuser` VALUES (1,'shweta'),(2,'None'),(3,'avni'),(4,'abc'),(5,'xyz'),(6,'anu'),(7,'pqr'),(8,'lmn'),(9,'vni');
+/*!40000 ALTER TABLE `currentuser` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-05 12:06:21
